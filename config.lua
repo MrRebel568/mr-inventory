df05042e90797f9d5360b755bf972bebb3d86b75
Mr Rebel <90243405+MrRebel568@users.noreply.github.com>
2022-12-30 02:50:55 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.MaxInventorySlots = 41 -- Max inventory slots for a player
 
 Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
 Config.MaxDropViewDistance = 12.5 -- The distance in GTA Units that a drop can be seen
-Config.UseItemDrop = true -- This will enable item object to spawn on drops instead of markers
+Config.UseItemDrop = false -- This will enable item object to spawn on drops instead of markers
 Config.ItemDropObject = `prop_nigel_bag_pickup` -- if Config.UseItemDrop is true, this will be the prop that spawns for the item
 
 Config.VendingObjects = {
